ed54cd5a8bd4513dc80a3402c1f525744adfaabe
Connor <ces156@pitt.edu>
2023-04-05 23:27:06 -0400

    Setup Navbar and Page Router, Page routes are not final

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,5 +1,9 @@
 import { createEffect, createSignal } from "solid-js";
 import Weather from "./components/Weather";
+import Error from "./components/Error";
+import LocationInput from "./components/LocationInput";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "@solidjs/router";
 
 //get input values
 const getWeather = () => {
@@ -24,41 +28,17 @@ const [weatherType, setWeatherType] = createSignal(1);
 function App() {
   return (
     <div class="bg-slate-900 min-h-screen">
-      {/* <p class="text-center p-2 text-slate-200">Weather App</p>
-      <div class="flex flex-row items-center justify-center mb-4">
-        <input class='mr-1 ml-5' type="radio" id="byLatLong" checked='checked' name="weatherType" value="1" onChange={radioOnChange}/>
-        <label class='text-slate-200' for="fahrenheit">Get Weather by Lat and Long</label>
-        
-        <input class='mr-1 ml-7 dark:text-white' type="radio" id="byLocation" name="weatherType" value="2" onChange={radioOnChange}/>
-        <label class='text-slate-200' for="celsius">Get Weather by Location</label>
+      <div>
+        <Navbar />
       </div>
-      <div id='inputDiv'>
-        {
-          weatherType() == 1 && (
-            <div>
-              <p class="text-center text-slate-200 mb-4">Enter Lat and Long of desired location</p>
-              <div class="flex flex-row items-center justify-center mb-4">
-                <input class="mr-5 ml-5 w-24 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50" type="text" placeholder="Latitude" id="LatitudeInput" ></input>
-                <input class="mr-5 w-24 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-5" type="text" placeholder="Longitude" id="LongitudeInput"></input>
-              </div>
-            </div>
-          ) 
-        }
-        {
-          weatherType() == 2 && (
-            <div>
-              <p class="text-center text-slate-200 mb-4">Enter Location of desired location</p>
-              <div class="flex flex-row items-center justify-center mb-4">
-                <input class="mr-5 ml-5 w-24 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50" type="text" placeholder="Location" id="LocationInput" ></input>
-              </div>
-            </div>
-          )
-        }
+      <div>
+          <Routes>
+            <Route path="/" component={LocationInput}/>
+            <Route path="/forecast" component={Weather}/>
+            <Route path="*" component={Error}/>
+
+          </Routes>
       </div>
-      <div class="flex flex-col items-center justify-center">
-        <button class="text-slate-200" onClick={getWeather}>Search</button>
-      </div> */}
-      <Weather />
     </div>
   );
 }
